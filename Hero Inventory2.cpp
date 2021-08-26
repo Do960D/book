4944c235fd39002d6212136eb28f6ce9b3a328e4
Твин Гарри <phuqup@bk.ru>
2021-08-26 10:55:19 +0300

    + some random into robber event

--- Hero Inventory2.cpp
+++ Hero Inventory2.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <ctime>
 
+
 using std::vector;
 using std::string;
 using std::cout;
@@ -15,10 +16,12 @@ int main()
 	inventory.push_back("armor");
 	inventory.push_back("shield");
 
+
 	cout << "You have " << inventory.size() << " items.\n";
 	cout << "\n Your items: \n";
 	for (unsigned int i = 0; i < inventory.size(); ++i)
 		cout << inventory[i] << endl;
+
 
 	cout << "\nYou trade your sword faor a battle axe.";
 	inventory[0] = "battle axe";
@@ -28,21 +31,31 @@ int main()
 	cout << "\The item name '" << inventory[0] << "' has ";
 	cout << inventory[0].size() << " letters in it.\n";
 
+
 	cout << "\nYour shild is destroyed in a fierce battle!";
 	inventory.pop_back();
 	cout << "\nYour items:\n";
+
 	for (unsigned int i = 0; i < inventory.size(); ++i)
 		cout << inventory[i] << endl;
+
 
 	cout << "\nYou were robbed of all of your possessions by a thief.\n";
 	inventory.clear();
 	srand(static_cast <unsigned int>(time(0)));
 	unsigned short luck = rand();
 	unsigned short yourLuck = (luck % 2);
+
 	if (yourLuck == 0)
 		cout << "\nYou have nothing.\n";
 	else
+	{
 		cout << "\nYou have at least one item\n";
+		inventory.push_back("sword");
+		cout << "\nYour items:\n";
+		for (unsigned int i = 0; i < inventory.size(); ++i)
+			cout << inventory[i] << endl;
+	}
 
-	return 0;
+return 0;
 }
