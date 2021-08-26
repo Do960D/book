f08e38ac703ed469e9bb41af65f8472da6109cb0
Твин Гарри <phuqup@bk.ru>
2021-08-26 17:21:16 +0300

    added Hero`s Inventory 3 (.cpp, example fom the book) with modification of last (after "armor destroyed") enumiration of items in inventory

---
+++ Hero Inventory3.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+int main()
+{
+
+	vector<string> inventory;
+	inventory.push_back("sword");
+	inventory.push_back("armor");
+	inventory.push_back("shield");
+
+	vector<string>::iterator myIterator;
+	vector<string>::const_iterator iter;
+
+	cout << "Your items: \n";
+	for (iter = inventory.begin(); iter != inventory.end(); ++iter)
+		cout << *iter << endl;
+
+	cout << "You trade your sword for a battle axe.";
+	myIterator = inventory.begin();
+	*myIterator = "battle axe";
+	cout << "\nYou items: \n";
+	for (iter = inventory.begin(); iter != inventory.end(); ++iter)
+		cout << *iter << endl;
+
+
+	cout << "\nThe item name '" << *myIterator << "' has ";
+	cout << (*myIterator).size() << " letters in it.\n";
+
+	cout << "\nThe item name '" << *myIterator << "' has ";
+	cout << myIterator->size() << " letters in it.\n";
+
+
+	cout << "\nYou recover a crossbow from a slain enemy";
+	inventory.insert(inventory.begin(), "crossbow");
+	cout << "\nYou items: \n";
+	for 
+		(iter = inventory.begin(); 
+		iter != inventory.end(); 
+		++iter)
+
+		cout << *iter << endl;
+
+	cout << "\nYour armor is destoyed in a fierce battle.";
+	inventory.erase((inventory.begin()+2));
+	cout << "\nYou items: \n";
+
+	iter = inventory.begin();
+	while 
+		( iter != inventory.end() )
+	{		
+		cout << *iter << endl;
+		++iter;
+	}
+
+	return 0;
+}
