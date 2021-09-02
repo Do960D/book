9ba7749ba96723636eba15bb9a34d3fd902870b2
Ilya <87614970+Do960D@users.noreply.github.com>
2021-09-02 15:29:40 +0300

    fix magic number (0) in -if "ALL"

--- favGameList.cpp
+++ favGameList.cpp
@@ -14,6 +14,7 @@ int main()
 {
 
 	const string commandForkMessage = "\nType ADD game name in list\nType DEL to remove game\nType ALL for view the list\nType EXIT to quit.\n";
+
 	string userInp, gameName;
 	vector<string> fvGameLst;
 	vector<string>::iterator kill;
@@ -34,29 +35,30 @@ int main()
 			fvGameLst.push_back(gameName);
 
 			cout << commandForkMessage;
-
 			cin >> userInp;
 			transform(userInp.begin(), userInp.end(), userInp.begin(), ::toupper);
-
-
-
 		}
 
 		if (userInp == "DEL")
 		{
-			//building in progress 
-
+			
 			/*
 			cout << "\nDeleting mode\n";
 			cin >> game;
 			kill = find(fvGameLst.begin(), fvGameLst.end(), fvGameLst);
 			cout << *kill;
 			*/
+
+			cout << "\nbuilding in progress...\n";
+			cout << commandForkMessage;
+			cin >> userInp;
+			transform(userInp.begin(), userInp.end(), userInp.begin(), ::toupper);
+
 		}
 
 		if (userInp == "ALL")
 		{
-			if (fvGameLst.size() != 0)
+			if (fvGameLst.size() != NULL)
 			{
 				cout << "\nFAVORITE GAMES LIST:\n";
 				for (unsigned int i = 0; i < fvGameLst.size(); i++)
