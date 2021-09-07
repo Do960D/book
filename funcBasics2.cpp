f897457e90a0c8c7195ab2dd935f2eb6aff9d200
Твин Гарри <phuqup@bk.ru>
2021-09-07 14:04:04 +0300

    add funcBasics2.cpp

---
+++ funcBasics2.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::cin;
+using std::string;
+
+
+int glob = 10;
+void regulMessage();
+void acceess_global();
+void hide_global();
+void change_global();
+
+
+int main()
+{
+	
+	regulMessage();
+	acceess_global();
+
+	hide_global();
+	regulMessage();
+
+	change_global();
+	regulMessage();
+	
+
+	return 0;
+}
+
+void regulMessage() 
+{
+	cout << "In main glob is: " << glob << endl;
+}
+
+void acceess_global()
+{
+	cout << "in access_global() glob is: " << glob << endl;
+}
+
+void hide_global() 
+{
+	int glob = 0; // hiding variable
+	cout << "In hide_global() glob is: " << glob << endl;
+}
+
+void change_global()
+{
+	glob = -10; // change variable
+	cout << "In change_global() glob is: " << glob << endl;
+}
