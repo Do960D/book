f48cd43080b0408da936209d698617c1a1f09801
Ilya <87614970+Do960D@users.noreply.github.com>
2021-09-08 18:10:31 +0300

    added funkBasics0.ccp

---
+++ funcBasics0.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int setDisp(int a, int b = 0);//prototype
+
+int main() 
+{
+	cout << setDisp(1,2); //caller NO DATA TYPE! //brasers () are contains funtion parameters  
+	
+	return 0;
+}
+
+
+int setDisp(int a, int b) // (arguments)
+{
+
+	a = rand();
+	int c = a + b;
+
+
+	return c, a, b;
+}
