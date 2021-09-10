138b41e8c4c4545af67e42c1fd1ff8729ef88050
Твин Гарри <phuqup@bk.ru>
2021-09-10 10:54:56 +0300

    add funcOverload

---
+++ funcOverload.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+#include<string>
+
+int triple(int number);
+std::string triple(std::string text);
+
+int main()
+{
+	std::cout << "Tripling 5: " << triple(5) << std::endl;
+	std::cout << "Tripling 'gamer': " << triple("gamer") << std::endl;
+
+	return 0;
+}
+
+int triple(int number)
+{
+	return (number * 3);
+}
+
+std::string triple(std::string text)
+{
+	return (text + text + text);
+}
